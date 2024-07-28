33132cbf211e13b54c418809a2a3dd79c82deb4a
ianrussel <ianrussel537@gmail.com>
2024-07-28 20:06:15 +0800

    update

--- access_token.py
+++ access_token.py
@@ -1,6 +1,16 @@
 import requests
+import os
+import sys
+from dotenv import load_dotenv
+import os
 
-api_token = "c7699b26b5a8e96874c8886ec545caeac99281f4"
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+dotenv_path = os.path.join(script_dir, '.env')
+load_dotenv(dotenv_path=dotenv_path, override=True)
+
+api_token = os.getenv('api_token')
 def get_accestoken():
     url = "https://cloud.seatable.io/api/v2.1/dtable/app-access-token/"
 
